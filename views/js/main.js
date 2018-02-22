45a701c547387bff99c8f1cd63745683f5ee5431
jangeliquem <35244042+jangeliquem@users.noreply.github.com>
2018-02-21 23:57:23 -0800

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -484,9 +484,10 @@ var resizePizzas = function(size) {
     //Changed to the faster document.getElementsByClassName() Web API call instead of document.querySelectorAll()
     //Took newwidth and dx variables out the loop (by commenting)
     //Created a local variable to save document.getElementsByClassName('randomPizzaContainer') outside the loop (e.g. var container = document.getElementsByClassName('randomPizzaContainer')), so the DOM is not explicitly touched in every iteration!
-    var randomPizzaContainer =  document.getElementsByClassName('randomPizzaContainer');
+    
     function changePizzaSizes(size) {
 //         moved newwidth and dx veriables out of the loop
+        var randomPizzaContainer =  document.getElementsByClassName('randomPizzaContainer');
         var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
         var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
         for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
