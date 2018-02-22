059378e7b8f2973d130c9c7d2b1d490c7bd4ba12
jangeliquem <35244042+jangeliquem@users.noreply.github.com>
2018-02-21 23:48:36 -0800

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -484,16 +484,16 @@ var resizePizzas = function(size) {
     //Changed to the faster document.getElementsByClassName() Web API call instead of document.querySelectorAll()
     //Took newwidth and dx variables out the loop (by commenting)
     //Created a local variable to save document.getElementsByClassName('randomPizzaContainer') outside the loop (e.g. var container = document.getElementsByClassName('randomPizzaContainer')), so the DOM is not explicitly touched in every iteration!
-    
+    var randomPizzaContainer =  document.getElementsByClassName('randomPizzaContainer');
     function changePizzaSizes(size) {
         for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+            var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
+            var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
             randomPizzaContainer[i].style.width = newwidth;
-//             var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
-//             var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
 //             document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
         }
     }
-    var randomPizzaContainer =  document.getElementsByClassName('randomPizzaContainer');
+    
     changePizzaSizes(size);
 
     // User Timing API is awesome
