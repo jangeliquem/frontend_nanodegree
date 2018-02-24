143d19873b6fb3af41ceff1322fb10f74b3df5c3
jangeliquem <35244042+jangeliquem@users.noreply.github.com>
2018-02-23 21:56:11 -0800

    Delete worker.js

--- views/js/worker.js
+++
@@ -1,8 +0,0 @@
-// This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
-  pizzasDiv.appendChild(pizzaElementGenerator(i));
-}//pizzaElementGenerator has the DOM traversal so moved it in here
-
-
-//randomPizzas and pizzaElementGenerator both accessible here?
